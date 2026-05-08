330308dcde5fcd822667f628097646dcb8fbfc27
akshita <akksut@gmail.com>
2026-05-09 02:02:51 +0530

    updates

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -92,7 +92,7 @@ function Sidebar() {
             </ul>
  
             <div className="sign">
-                <p>By ApnaCollege &hearts;</p>
+                <p>By PromptlyAI &hearts;</p>
             </div>
         </section>
     )
